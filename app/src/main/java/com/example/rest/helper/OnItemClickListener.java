1721f46cf87a63d0858797e837bf447b34827738
bayu-17ajid <bayuajid12@gmail.com>
2021-05-09 18:59:37 +0700

    update

---
+++ app/src/main/java/com/example/rest/helper/OnItemClickListener.java
@@ -0,0 +1,5 @@
+package com.example.rest.helper;
+
+public interface OnItemClickListener<T> {
+    void onClick(T t);
+}
---
+++ app/src/main/java/com/example/rest/networks/Consts.java
@@ -0,0 +1,12 @@
+package com.example.rest.networks;
+
+public class Consts {
+
+    public static final String BASEURL = "https://api.themoviedb.org/3/";
+    public static final String APIKEY = "1dbd7a6156caed3bfafb1cb41d6b0243";
+    public static final String LANGUAGE = "en-US";
+    public static final String PAGE = "1";
+    public static final String IMAGEBASEURL = "https://image.tmdb.org/t/p/w500/";
+    public static final String APIERROR = "DEBUG:API ERROR";
+
+}
